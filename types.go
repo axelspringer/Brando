11212b0bbdb970109cc94cb9dc47fd77fe21e967
TetsuyaXD <33517730+TetsuyaXD@users.noreply.github.com>
2018-05-16 11:19:39 +0200

    Update types.go

--- types.go
+++ types.go
@@ -11,8 +11,8 @@ type LiveEvent struct {
 	titel string`json:"titel"`
 	presentor string`json:"presentor"`
 	description string`json:"description"`
-    date_begin string`json:"date_begin"`
-	date_end string`json:"date_end"`
+    	dateBegin string`json:"date_begin"`
+	dateEnd string`json:"date_end"`
 	live bool`json:"live"`
 	featured bool`json:"featured"`
 }
